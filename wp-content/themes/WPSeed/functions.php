396b911e36929a0d3ca8b845de6c28f4270d4f81
Emily <emily.a.morgan00@gmail.com>
2018-08-06 12:26:24 -0400

    Increase media upload size in functions.php

--- wp-content/themes/WPSeed/functions.php
+++ wp-content/themes/WPSeed/functions.php
@@ -11,6 +11,10 @@
  *
  */
 
+@ini_set( 'upload_max_size' , '128M' );
+@ini_set( 'post_max_size', '128M');
+@ini_set( 'max_execution_time', '300' );
+
 /* ACCESS CONTROL
 /===================================================== */
   require('functions/functions-access.php');
