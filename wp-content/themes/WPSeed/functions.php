bcb5e99f187d42b7d2b847e4778e0df84267e4db
Emily <emily.a.morgan00@gmail.com>
2018-08-06 12:43:51 -0400

    Revert php ini upload limit increase

--- wp-content/themes/WPSeed/functions.php
+++ wp-content/themes/WPSeed/functions.php
@@ -11,10 +11,6 @@
  *
  */
 
-@ini_set( 'upload_max_size' , '128M' );
-@ini_set( 'post_max_size', '128M');
-@ini_set( 'max_execution_time', '300' );
-
 /* ACCESS CONTROL
 /===================================================== */
   require('functions/functions-access.php');
